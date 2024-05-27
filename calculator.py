81b186d00048fddd2175c722db44a515f14f66b1
Kaspars <smarnss@gmail.com>
2024-05-27 17:18:31 +0300

    Add internal files to github.

---
+++ calculator.py
@@ -0,0 +1,19 @@
+import random
+from functions import calculate
+
+
+
+print("Welcome to Simple Calculator")
+result: None = None
+number_1 = input("enter number_1: ")
+operation = input("Choose operator: + - * /  ")
+number_2 = input("enter number_2: ")
+
+if number_1.isnumeric() and number_2.isnumeric():
+    print('you enter the currect digit')
+else:
+    exit("incorrect format")
+
+result = calculator(operation, number_1, number_2)
+
+print(result)
